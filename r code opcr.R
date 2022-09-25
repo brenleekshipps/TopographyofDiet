a059807fab63f3362e64f87796ad9b2fb6d53388
brenleekshipps <114366169+brenleekshipps@users.noreply.github.com>
2022-09-25 18:09:02 -0400

    Update r code opcr.R

--- r code opcr.R
+++ r code opcr.R
@@ -8,29 +8,3 @@ OPC(x, minimum_area = 0.01)
 OPCx_output <- OPC(x, minimum_area = 0.01)
 OPC3d (OPCx_output, maskDiscard = TRUE, binColors = palette1, fileName='test')
 OPCr (x, minimum_area = 0.01)
-
-
-
-xfile <- file.choose()
-x <- read.ply(xfile)
-DNE(x, outliers = 0.1, BoundaryDiscard = "Vertex")
-DNE3d(dneoutput)
-
-
-xfile <- file.choose()
-x <- read.ply(xfile)
-RFI_output <- RFI(x, alpha=0.5)
-RFI3d(
-  RFI_output,
-  displacement = -1.9,
-  SurfaceColor = "gray",
-  FootColor = "red",
-  FootPts = FALSE,
-  FootPtsColor = "black",
-  Opacity = 1,
-  legend = F,
-  legendScale = 1,
-  leftOffset = 0,
-  fieldofview = 0
-)
-Check2D(RFI_output)
